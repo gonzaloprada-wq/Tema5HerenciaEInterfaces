fc1368eb0700676e2c313b3a02b869bae73135b1
GonzaloPG <gonzalo.prada@iesnervion.es>
2026-04-10 13:27:01 +0200

    10042610453

---
+++ TEMA5/src/InterfacesEj1/Socio.java
@@ -0,0 +1,76 @@
+package InterfacesEj1;
+
+public class Socio implements Comparable{
+
+	private static final Socio Object = null;
+
+	private int id;
+	
+	private String nombre;
+	
+	private int edad;
+	
+	
+	
+	
+	public Socio(int id, String nombre, int edad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+	@Override
+	public int compareTo(Object o) {
+
+		Socio otroSocio=(Socio) o;	
+		
+		return Integer.compare(this.id, otroSocio.id);
+	}
+	
+}
+
+
--- TEMA5/src/InterfacesEj3/Gato.java
+++ TEMA5/src/InterfacesEj3/Gato.java
@@ -27,4 +27,6 @@ public class Gato extends AnimalDomestico {
 	}
 	
 	
+	
+	
 }
